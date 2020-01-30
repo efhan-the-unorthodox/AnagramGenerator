b2908be54d1b0e51a11708687ef55121609e2568
ETHAN LEO MING QUAN TP <1800249C@student.tp.edu.sg>
2020-01-30 16:30:33 +0800

    Add project files.

---
+++ AnagramApp/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace AnagramApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbRandChar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRandomCharacters = new System.Windows.Forms.Button();
+            this.generateWords = new System.Windows.Forms.Button();
+            this.lbAnagrams = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Orange;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 71);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anagram";
+            // 
+            // tbRandChar
+            // 
+            this.tbRandChar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRandChar.Location = new System.Drawing.Point(235, 112);
+            this.tbRandChar.Name = "tbRandChar";
+            this.tbRandChar.Size = new System.Drawing.Size(299, 34);
+            this.tbRandChar.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Characters";
+            // 
+            // btnRandomCharacters
+            // 
+            this.btnRandomCharacters.Location = new System.Drawing.Point(235, 169);
+            this.btnRandomCharacters.Name = "btnRandomCharacters";
+            this.btnRandomCharacters.Size = new System.Drawing.Size(106, 34);
+            this.btnRandomCharacters.TabIndex = 3;
+            this.btnRandomCharacters.Text = "Random";
+            this.btnRandomCharacters.UseVisualStyleBackColor = true;
+            this.btnRandomCharacters.Click += new System.EventHandler(this.randomCharacters_Click);
+            // 
+            // generateWords
+            // 
+            this.generateWords.Location = new System.Drawing.Point(363, 169);
+            this.generateWords.Name = "generateWords";
+            this.generateWords.Size = new System.Drawing.Size(106, 34);
+            this.generateWords.TabIndex = 4;
+            this.generateWords.Text = "Generate";
+            this.generateWords.UseVisualStyleBackColor = true;
+            this.generateWords.Click += new System.EventHandler(this.generateWords_Click);
+            // 
+            // lbAnagrams
+            // 
+            this.lbAnagrams.FormattingEnabled = true;
+            this.lbAnagrams.ItemHeight = 16;
+            this.lbAnagrams.Location = new System.Drawing.Point(235, 242);
+            this.lbAnagrams.Name = "lbAnagrams";
+            this.lbAnagrams.Size = new System.Drawing.Size(299, 196);
+            this.lbAnagrams.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbAnagrams);
+            this.Controls.Add(this.generateWords);
+            this.Controls.Add(this.btnRandomCharacters);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbRandChar);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Anagram";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbRandChar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRandomCharacters;
+        private System.Windows.Forms.Button generateWords;
+        private System.Windows.Forms.ListBox lbAnagrams;
+    }
+}
+
---
+++ AnagramApp/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnagramApp
+{
+    public partial class Form1 : Form
+    {
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //The code here will generate 10 random characters
+        private void randomCharacters_Click(object sender, EventArgs e)
+        {
+            //clear the initial random letters
+            tbRandChar.Clear();
+
+            string tenChars = "";
+
+            //create a string with all the alphabets
+            string alphabets = "abcdefghijklmnopqrstuvwxyz";
+            Random random = new Random();
+
+            for(var i =0; i <10; i++)
+            {
+                //get a random index that will represent the alphabet in the string of alphabets
+                var randomIndex = random.Next(0, 26);
+
+                tenChars += alphabets[randomIndex];
+            }
+
+            tbRandChar.Text = tenChars;
+
+        }
+
+        private void generateWords_Click(object sender, EventArgs e)
+        {
+
+            var anagramList = new List<string>();
+
+            //list of alphabets
+            string alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+            var alphaList = alphabets.ToList();
+
+            //get all of the random characters
+            var randChars = tbRandChar.Text;
+            
+
+            //convert to character array
+            var ranCharArr = randChars.ToCharArray();
+            //sort the characters
+            Array.Sort(ranCharArr);
+
+            //in the event of duplicate characters, create an array with all unique characters
+            var ranCharArrUnique = ranCharArr.Distinct();
+
+            //loop through the random characters and remove them from the alphabets
+            //The objective here is to get all the characters that are not present in the random letters generated
+            foreach(var character in ranCharArrUnique)
+            {
+                alphaList.Remove(character);
+            }
+
+            //call the file for the dictionary of words
+            var allWords = System.IO.File.ReadAllLines($@"D:\WORLD SKILLS\AnagramApp\words_alpha.txt");
+
+            //given that are 10 characters, get only the words that are maximum 10 characters long
+            var possibleWords = allWords.Where(a => a.Length <= randChars.Length).ToList();
+
+            //all characters that should be excluded when finding anagrams
+            var excludeCharacters = alphaList.ToArray();
+            
+            //remove all words that have the characters not found in the 10 random characters
+            foreach(var exclude in excludeCharacters)
+            {
+                possibleWords.RemoveAll(a => a.Contains(exclude));
+            }
+
+            ///check through the random characters and see their count
+            ///the objective here is to find the words that have characters that are higher than the stipulated number in the random generated
+            foreach(var ranLetter in ranCharArrUnique)
+            {
+                //get the letter that is being checked
+                var checkLetter = ranLetter;
+                //get the count of that letter in the bunch of randomly generated characters
+                var charCount = ranCharArr.Where(a => a.Equals(checkLetter)).Count();
+
+                //remove all words where the count of the specified character is higher than the number of the character randomly given
+                var filterWords = possibleWords.Where(a => a.Contains(checkLetter)).ToList();
+                foreach(var word in filterWords)
+                {
+                    var count = word.Where(a => a.Equals(checkLetter)).Count();
+                    if(count > charCount)
+                    {
+                        possibleWords.Remove(word);
+                    }
+                }
+            }
+
+            foreach(var a in possibleWords)
+            {
+                anagramList.Add(a);
+            }
+
+            lbAnagrams.DataSource = new BindingSource(anagramList.OrderBy(a => a.Count()), null);
+        }
+
+    }
+}
